ad6fd79321dddd1cde3a970e5ab5ddf26759e4dd
Marian Schön <marian.schoen@stud.uni-regensburg.de>
2018-07-24 11:31:43 +0200

    trace gradient smaller changes

--- R/function_gradient.R
+++ R/function_gradient.R
@@ -3,12 +3,12 @@ library(Matrix)
 
 #' Title
 #'
-#' @param X 
-#' @param Y 
-#' @param C 
-#' @param gamma.vec 
+#' @param X
+#' @param Y
+#' @param C
+#' @param gamma.vec
 #'
-#' @return numeric list, same length as "gamma.vec" 
+#' @return numeric list, same length as "gamma.vec"
 #' @export
 #'
 #' @examples Trace.H.gradient(tweak)
@@ -25,8 +25,8 @@ Trace.H.gradient <- function(X = x_mat, Y = y_mat, C = c_mat, gamma.vec = v_vec)
   for(j in 1:nrow(C)){
     cov.cs.hat.cs <- c(cov.cs.hat.cs, cov(estimates.cs[j,],C[j,]) )
   }
-  
-  A <- matrix(NA, nrow=ncol(X), ncol=ncol(C))  
+
+  A <- matrix(NA, nrow=ncol(X), ncol=ncol(C))
   for(j in 1:ncol(X)){
     faktor <- 1/(hat.sigmas[j] * sigmas[j])
     cov.part <- cov.cs.hat.cs[j] / (N * hat.sigmas[j]^2)
@@ -34,13 +34,13 @@ Trace.H.gradient <- function(X = x_mat, Y = y_mat, C = c_mat, gamma.vec = v_vec)
       A[j, k] <- faktor * (cov.part * (estimates.cs[j,k] - mean.hat.cs[j]) - ((C[j,k] - mean.cs[j])/N))
     }
   }
-  
-  
+
+
   alpha <- solve(t(X)%*%Gamma%*%X)%*%t(X)
   beta <- (diag(1, nrow=nrow(Y), ncol=nrow(Y)) - X %*% alpha %*% Gamma) %*% Y
-  
+
   B <- beta %*% t(A) %*% alpha
   d.elems <- diag(B)
-  
+
   return(d.elems)
 }
