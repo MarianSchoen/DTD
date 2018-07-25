a9ec635854657bbececceddc1aca200d3c41da5b
Marian Schön <marian.schoen@stud.uni-regensburg.de>
2018-07-25 09:24:54 +0200

    descent cycle version as suggested within Franzis algorithm

--- R/function_FISTA.R
+++ R/function_FISTA.R
@@ -79,7 +79,6 @@
 #' special.samples <- c("Type1")
 #' all.samples <- unique(indicator.list)
 #' sample.names <- all.samples[- which(all.samples %in% special.samples)]
-#'
 #' training.data <- mix.samples.Jitter(sample.names = sample.names,
 #'                                      special.samples = special.samples,
 #'                                      nMixtures = 1e3,
@@ -117,24 +116,24 @@
 #'                                    F.GRAD.FUN = DTD.grad.wrapper,
 #'                                    ST.FUN = soft_thresholding,
 #'                                    FACTOR.FUN = nesterov_faktor,
-#'                                    EVAL.FUN = DTD.evCor.wrapper)
+#'                                    EVAL.FUN = DTD.evCor.wrapper,
+#'                                    line_search_speed = 2,
+#'                                    maxit = 1e3)
 #'
 #'
 #'
 #'
 descent_generalized_fista <- function(tweak_vec = NA,
                                       lambda=0,
-                                      maxit=100,
+                                      maxit=1e3,
                                       learning.rate = NA,
                                       F.GRAD.FUN = DTD.grad.wrapper,
                                       ST.FUN = soft_thresholding,
                                       FACTOR.FUN = nesterov_faktor,
                                       EVAL.FUN = DTD.evCor.wrapper,
-                                      line_search_speed = 1.01,
-                                      tol = 1e-5,
+                                      line_search_speed = 2,
+                                      cycles=50,
                                       verbose=T){
-  # if not set, set learning rate to 1.
-  # The idea would be that there are intelligent ways to approximate the best learning rate. (see e.g. apgpy)
   if(is.na(learning.rate)){
     # estimating best step size using barzilai borwein initialization
     # adapted from apgpy !
@@ -144,51 +143,53 @@ descent_generalized_fista <- function(tweak_vec = NA,
     x_hat <- tweak_vec - t * grad
     g_hat <- F.GRAD.FUN(x_hat)
     learning.rate <- abs((tweak_vec - x_hat) %*% (grad - g_hat) / sum((grad - g_hat)**2))
+    # learning rate is a matrix => make it a numeric:
+    learning.rate <- as.numeric(learning.rate)
   }
 
   # safety check.
-  # Actually, the idea was to check if the user predefined tweak_vec vector does not fit the data. But therefore the algorithm needs to know the f-function. (E.g. dimension c in ||y-Xc|| and dimension of g of -sum(cor(c, c_g)) are not the same)
   if(any(is.na(tweak_vec))){
     stop("Tweak vector includes NAs")
   }
-  lsspeed_safe <- line_search_speed
-  # In each iteration the updated tweak_vec will be added to tweak.history. This matrix will be returned after all iterations.
-  # tweak.history is being initialised with two replicates of the input tweak_vec, because of the nesterov step.
+
   tweak_old <- tweak_vec
   converge_vec <- EVAL.FUN(tweak_vec)
   y_vec <- tweak_vec
   factor <- FACTOR.FUN(2)
-  cat("starting to optimize \n")
+  if(verbose){
+    cat("starting to optimize \n")
+  }
+
+  # step size sequence:
+  sequence <- seq(from = 0, to = learning.rate, length.out = cycles)
+
+
   for(iter in 2:maxit){
     grad <- F.GRAD.FUN(y_vec)
 
-    u_vec <- ST.FUN(y_vec - learning.rate* grad, learning.rate*lambda)
-    eval <- EVAL.FUN(u_vec)
-    i <- 1
-    switched <- TRUE
-    while(converge_vec[iter - 1] < eval){ # did not decrease
-      l.rate <- learning.rate/line_search_speed^i
-      u_vec <- ST.FUN(y_vec - l.rate*grad, l.rate*lambda)
-      summary(u_vec)
-      eval.old <- eval
-      eval <- EVAL.FUN(u_vec)
-      if(i %% 100 == 0){
-        cat("line searching step: ", i, " in ", iter, " \n")
-      }
-      i <- i + 1
-      if(abs(eval.old - eval) < tol && switched && i > 100){ # means that decreasing learning rate does not change the result
-        line_search_speed <- 1/line_search_speed
-        i <- 2
-        cat("switched\n")
-        switched <- FALSE
-      }
-      if( l.rate > 1e50){ # honestly, here i don't know what to do ...
-        cat("Can't increase Loss anymore\n")
-        ret <- list("Tweak"=tweak_vec, "Convergence"=converge_vec)
-        return(ret)
-      }
+
+    # u_mat will be a matrix with new u_vecs in each row
+    u_mat <- matrix(unlist(lapply(sequence,
+                           function(x){ST.FUN(y_vec - x * grad, x*lambda)}),
+                    use.names = FALSE), nrow = cycles, byrow = T )
+
+    # use evaluation function on each row => eval.vec is a vector with Loss-function values
+    eval.vec <- apply(u_mat, 1, EVAL.FUN)
+
+    winner.pos <- which.min(eval.vec)
+
+    if(winner.pos == cycles){
+      learning.rate <- learning.rate * line_search_speed
+      sequence <- seq(from = 0, to = learning.rate, length.out = cycles)
     }
-    line_search_speed <- lsspeed_safe
+    if(winner.pos == 1){
+      learning.rate <- learning.rate / line_search_speed
+      sequence <- seq(from = 0, to = learning.rate, length.out = cycles)
+    }
+
+    u_vec <- u_mat[winner.pos, ]
+    eval <- eval.vec[winner.pos]
+
     converge_vec <- c(converge_vec, eval)
     tweak_old <- tweak_vec
 
