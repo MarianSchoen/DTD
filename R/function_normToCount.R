3cc8be03c44c7db0c63a07251e16412441381247
Marian Schön <marian.schoen@stud.uni-regensburg.de>
2018-07-30 10:40:03 +0200

    new description

--- R/function_normToCount.R
+++ R/function_normToCount.R
@@ -1,26 +1,28 @@
 #' normalizeToCount
-#' 
-#' 'normalizeToCount' takes a numeric matrix mat and normalizes each column (=> sample) to fix number of counts
+#'
+#' 'normalizeToCount' takes a numeric matrix mat and normalizes each column (=> sample) to fix number of counts.\cr
+#' For every sample i, each feature j gets scaled to
+#'  \deqn{mat[j, i] = (count * mat[j, i]) / sum(mat[, i])}
 #'
 #' @param mat numeric matrix, with samples as columns and features as columns
-#' @param count float 
+#' @param count float , to which every sample of mat will be scaled
 #'
-#' @return : "ret", matrix with same dimension as "mat"
+#' @return "ret", matrix with same dimension as "mat"
 #' @export
 #'
-#' @examples 
+#' @examples
 #' someMatrix <- matrix(rnorm(1000*5), ncol=5, nrow=1000)
-#' # each sample (=column) has different sum: 
+#' # each sample (=column) has different sum:
 #' apply(someMatrix, 2, sum)
-#' 
+#'
 #' normalizedMatrix <- normalizeToCount(someMatrix)
-#' 
-#' # check: 
+#'
+#' # check:
 #' apply(normalizedMatrix, 2, sum)
 normalizeToCount <- function(mat, count=1e6){
-  # get all sum over all columns: 
+  # get all sum over all columns:
   sums <- colSums(mat)
-  
+
   # for each column ...
   for(l1 in 1:ncol(mat)){
     # ... norm each column by its sum, then multiply each entry with "count"
