222bfa5e7ffbd7ca46da06fdf8a12cef0253d278
Marian Schön <marian.schoen@stud.uni-regensburg.de>
2018-07-31 12:00:58 +0200

    est.cs final version (or at least I hope that it is the final version)

--- R/function_estimateC.R
+++ R/function_estimateC.R
@@ -1,16 +1,21 @@
 #' Estimating C
 #'
-#' Given a reference matrix X, a matrix of bulks and a g-vector, est.cs finds the solution of
-#' \deqn{arg min || diag(g) (Y - XC) ||_2}
-#' over all C using direct analytical solution
+#' Given a reference matrix X, a matrix of bulks and a g-vector, "est.cs" finds
+#' the solution of \deqn{arg min || diag(g) (Y - XC) ||_2} over all C using
+#' direct analytical solution:
+#' \deqn{ C(g) = (X^T \Gamma X )^(-1) X^T \Gamma Y}
 #'
-#' @param X numeric matrix with cells as columns, and features as rows. Reference matrix X of the DTD problem.
+#'
+#' @param X numeric matrix with cells as columns, and features as rows.
+#'   Reference matrix X of the DTD problem.
 #' @param Y numeric matrix with samples as columns, and features as rows.
-#' @param gamma.vec numeric vector with length of nrow(X). In the equation above the gamma.vec is denotated as g.
+#' @param gamma.vec numeric vector with length of nrow(X). In the equation above
+#'   the gamma.vec is denotated as g.
 #'
 #' @return numeric matrix with ncol(X) rows, and ncol(Y) columns
 #'
 #' @export
+#'
 #'
 #' @examples
 #' library(DTD)
@@ -34,9 +39,16 @@
 #'
 #' # visualize that the estimated c are close to the true c
 #' plot(true.c, estimated.c)
+#'
 est.cs <- function(X, Y, gamma.vec){
+
+  if(length(gamma.vec) != nrow(X) || nrow(X) != nrow(Y)){
+    stop("est.cs: dimension of provided input does not match")
+  }
+
+  # transform gamma.vec into a diagonal matrix (everything but diagonal is zero)
   Gamma <- diag(gamma.vec)
-  temp.sol <- solve(t(X)%*%Gamma%*%X)%*%t(X)%*%Gamma
-  sol <- temp.sol%*%Y
+  # anallytical solution from formula in description:
+  sol <- solve(t(X)%*%Gamma%*%X)%*%t(X)%*%Gamma %*% Y
   return(sol)
 }
