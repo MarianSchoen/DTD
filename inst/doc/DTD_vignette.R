b70a0018a07d2d24cdc0d5ee99a9a550fdf564a8
Marian Schön <marian.schoen@stud.uni-regensburg.de>
2018-08-21 10:25:54 +0200

    vignette new structure

--- inst/doc/DTD_vignette.R
+++ inst/doc/DTD_vignette.R
@@ -6,8 +6,98 @@ knitr::opts_chunk$set(
 
 ## ----include=FALSE-------------------------------------------------------
   # for fast testing 
-  maxit <- 250
+  maxit <- 100
   nSamples <- 500
+
+## ----include=FALSE-------------------------------------------------------
+  # I'd like to start with 'training the g vector'. Therefore I need a lot of stuff ...
+  library(DTD)
+  number.types <- 10
+  random.data <- generate.random.data(nTypes = number.types, 
+                                      nSamples.perType = 150, 
+                                      nFeatures = 250)
+  normalized.data <- normalizeToCount(random.data)
+  indicator.list <- gsub("^Cell[0-9]*\\.", "", colnames(normalized.data))
+  names(indicator.list) <- colnames(normalized.data)
+  include.in.X <- paste0("Type", 2:5)
+  X.matrix <- matrix(NA, nrow=nrow(normalized.data), ncol=length(include.in.X))
+  colnames(X.matrix) <- include.in.X
+  rownames(X.matrix) <- rownames(normalized.data)
+    percentage.of.all.cells <- 0.2
+  samples.to.remove <- c()
+  for(l.type in include.in.X){
+    # get sample names of all cells of type "l.type" 
+    all.of.type <- names(indicator.list)[which(indicator.list == l.type)]
+    
+    # randomly sample some cells
+    chosen.for.X <- sample(x = all.of.type,
+                           size = ceiling(length(all.of.type) * percentage.of.all.cells),
+                           replace = FALSE)
+    
+    # Add those cells which will be included in X to the list of samples.to.remove 
+    samples.to.remove <- c(samples.to.remove, chosen.for.X)
+
+    # for each gene average over the selected 
+    average <- rowSums(normalized.data[, chosen.for.X, drop = FALSE])
+    X.matrix[, l.type] <- average
+  }
+  remaining.mat <- normalized.data[, -which(colnames(normalized.data) %in% samples.to.remove)]
+  train.samples <- sample(x = colnames(remaining.mat), 
+                          size = ceiling(ncol(remaining.mat)/2), 
+                          replace = FALSE)
+  test.samples <- colnames(remaining.mat)[which(!colnames(remaining.mat) %in% train.samples)]
+  
+  train.mat <- remaining.mat[, train.samples]
+  test.mat <- remaining.mat[, test.samples]
+  indicator.train <- indicator.list[names(indicator.list) %in% colnames(train.mat)]
+  training.data <- mix.samples(gene.mat = train.mat,
+                               pheno = indicator.train,
+                               included.in.X = include.in.X, 
+                               nSamples = nSamples, 
+                               nPerMixture = 100, 
+                               verbose = F)
+  indicator.test <- indicator.list[names(indicator.list) %in% colnames(test.mat)]
+  test.data <- mix.samples(gene.mat = test.mat,
+                           pheno = indicator.test,
+                           included.in.X = include.in.X, 
+                           nSamples = nSamples, 
+                           nPerMixture = 100, 
+                           verbose = F)
+  # wrapper for gradient:
+   DTD.grad.wrapper <- function(tweak){
+      X <- X.matrix
+      Y <- training.data$mixtures
+      C <- training.data$quantities
+      grad <- Trace.H.gradient(X = X, Y = Y, C = C, tweak = tweak)
+      return(grad)
+   }
+  # wrapper for evaluation:
+  DTD.evCor.wrapper <- function(tweak){
+      X <- X.matrix
+      Y <- training.data$mixtures
+      C <- training.data$quantities
+      loss <- evaluate_cor(X = X, Y = Y, C = C, tweak = tweak)
+      return(loss)
+  }
+  start_tweak <- rep(1, nrow(X.matrix))
+  start.loss <- DTD.evCor.wrapper(start_tweak)
+
+
+## ---- fig.width = 7------------------------------------------------------
+  catch <- descent_generalized_fista(tweak_vec = start_tweak,
+                                     F.GRAD.FUN = DTD.grad.wrapper,
+                                     ST.FUN = soft_thresholding,
+                                     FACTOR.FUN = nesterov_faktor,
+                                     EVAL.FUN = DTD.evCor.wrapper,
+                                     line_search_speed = 2,
+                                     maxit = maxit,
+                                     save_all_tweaks = T, 
+                                     verbose = F)
+  str(catch)
+  print(ggplot_correlation(fista.output = catch, 
+                         test.set = test.data, 
+                         X.matrix = X.matrix, 
+                         main = "DTD Vignette"))
 
 ## ------------------------------------------------------------------------
   library(DTD)
@@ -28,9 +118,6 @@ knitr::opts_chunk$set(
   print(colnames(random.data)[example.index])
 
 ## ------------------------------------------------------------------------
-    random.data <- 2^random.data - 1
-
-## ------------------------------------------------------------------------
   # In random.data the number of counts differs over all samples: 
   apply(random.data, 2, sum)[example.index]
   normalized.data <- normalizeToCount(random.data)
@@ -43,7 +130,7 @@ knitr::opts_chunk$set(
  print(indicator.list[example.index])
 
 ## ------------------------------------------------------------------------
-  include.in.X <- paste0("Type", 2:7)
+  include.in.X <- paste0("Type", 2:5)
   print(include.in.X)
 
 ## ------------------------------------------------------------------------
@@ -139,6 +226,8 @@ knitr::opts_chunk$set(
 
 ## ------------------------------------------------------------------------
   start_tweak <- rep(1, nrow(X.matrix))
+  start.loss <- DTD.evCor.wrapper(start_tweak)
+  cat("Start average correlation: ", -start.loss/ncol(X.matrix))
   catch <- descent_generalized_fista(tweak_vec = start_tweak,
                                      F.GRAD.FUN = DTD.grad.wrapper,
                                      ST.FUN = soft_thresholding,
