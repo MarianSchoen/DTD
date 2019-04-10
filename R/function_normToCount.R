11be5aa8ce45a6010a6cdf5f6c9e33d908da8623
Marian Schön <marian.schoen@stud.uni-regensburg.de>
2019-04-10 09:21:03 +0200

    apply instead of loop => final version

--- R/function_normToCount.R
+++ R/function_normToCount.R
@@ -1,16 +1,19 @@
 #' normalize_to_count
 #'
-#' 'normalize_to_count' takes a numeric matrix 'exp.data' with only non-negative entries and normalizes each column (=> sample) to a fix number of counts.\cr
+#' 'normalize_to_count' takes a numeric matrix 'exp.data' with only non-negative
+#' entries and normalizes each column (=> sample) to a total number of counts.\cr
 #' For every sample i, each feature j gets scaled to
 #'  \deqn{exp.data[j, i] = (count * exp.data[j, i]) / sum(exp.data[, i])}
 #'
-#' @param exp.data positive numeric matrix, with samples as columns and features as columns
-#' @param count float , to which every sample of exp.data will be scaled
+#' @param exp.data positive numeric matrix, with samples as columns and features as rows.
+#' Notice, 'normalize_to_count' normalizes the columns of this matrix.
+#' @param count float, 0 < 'count', to which every sample of exp.data will be scaled
 #'
 #' @return "ret", matrix with same dimension as "exp.data"
 #' @export
 #'
 #' @examples
+#' library(DTD)
 #' some.matrix <- matrix(abs(rnorm(1000 * 5)), ncol = 5, nrow = 1000)
 #' # each sample (=column) has different number of total counts:
 #' apply(some.matrix, 2, sum)
@@ -40,13 +43,9 @@ normalize_to_count <- function(exp.data, count = 1e6) {
   }
   # end -> exp.data
 
-  # get all sum over all columns:
-  sums <- colSums(exp.data)
+  # microbenchmark told me that apply is faster than a loop,
+  # therefore normalizing is done via apply:
+  norm.data <- apply(exp.data, 2, function(x){count*x/sum(x)})
 
-  # for each column ...
-  for (l1 in 1:ncol(exp.data)) {
-    # ... norm each column by its sum, then multiply each entry with "count"
-    exp.data[, l1] <- count * exp.data[, l1] / sums[l1]
-  }
-  return(exp.data)
+  return(norm.data)
 }
