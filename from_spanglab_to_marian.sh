66256bdd784fbcd850bf8a8e3b4901e098d5105f
Marian Schön <marian.schoen@stud.uni-regensburg.de>
2019-04-17 16:19:01 +0200

    for syncing it to MarianSchon github

---
+++ from_spanglab_to_marian.sh
@@ -0,0 +1,7 @@
+exclude#!/bin/bash
+
+PATH_TO="/Users/marian/Documents/"
+SOURCE="${PATH_TO}/git/DTD_spanglab"
+TARGET="${PATH_TO}/git/DTD"
+
+rsync -av ${SOURCE}/ ${TARGET}/ --exclude-from=exclude_from_rsync
