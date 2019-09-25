e3bdb66c870fc8e8d1946d5a178dd3244414a00b
Marian Schön <marian.schoen@stud.uni-regensburg.de>
2019-09-25 12:39:10 +0200

    outsourced and roxygen skeleton checked

---
+++ R/function_cv_pick_mean_test.R
@@ -0,0 +1,17 @@
+#' returns mean of the individual test results, setting unconverged or unavailable results to Inf.
+#'
+#' @param lambda.list output of a DTD cross validation run (list of lists)
+#'
+#' @return list of means.
+#'
+pick.mean.test.results.function <- function(lambda.list){
+  tmp <- lapply(lambda.list, function(each.fold){
+    if("cor.test" %in% names(each.fold)){
+      return(each.fold$cor.test)
+    }else{
+      return(Inf)
+    }
+  })
+  test.vec <- mean(unlist(tmp, use.names = FALSE), na.rm = TRUE)
+  return(test.vec)
+}
