2d007787cd94fd0abcca1ab544b1992f562dc402
Jakob Simeth <jakob.simeth@physik.uni-regensburg.de>
2019-11-04 15:07:41 +0100

    rename faktor -> factor.

--- R/function_nesterovFactors.R
+++ R/function_nesterovFactors.R
@@ -1,6 +1,6 @@
-#' Nesterov Faktor 2/(k+1)
+#' Nesterov Factor 2/(k+1)
 #'
-#' The FISTA algorithm extrapolates/correctes after the gradient step. The "nesterov_faktor" function
+#' The FISTA algorithm extrapolates/correctes after the gradient step. The "nesterov_factor" function
 #' returns how far the FISTA will exptrapolate in a given step "k"
 #'
 #' @param k integer, iteration step of the FISTA algorithm
@@ -8,10 +8,10 @@
 #' @return integer
 #'
 #' @examples
-#' nesterov_faktor(2)
-nesterov_faktor <- function(k) {
+#' nesterov_factor(2)
+nesterov_factor <- function(k) {
   test <- test_integer(test.value = k,
-                       output.info = c("nesterov_faktor", "k"),
+                       output.info = c("nesterov_factor", "k"),
                        min = 1,
                        max = Inf)
   return(2 / (k + 1))
