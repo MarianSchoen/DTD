f21d183e416a5546b18b8497beb4aaa65943abb3
Marian Schön <marian.schoen@stud.uni-regensburg.de>
2019-11-18 11:49:29 +0100

    added an example
    (the not exported function don't need a deep explanation, however, a short summary will be included)

--- R/function_cv_bucket_maker.R
+++ R/function_cv_bucket_maker.R
@@ -2,11 +2,20 @@
 #'
 #' takes training data and sorts it into buckets, one for each fold.
 #'
-#' @param train.Y training data
-#' @param folds number of buckets to build.
+#' @param train.Y numeric matrix, training data
+#' @param folds integer, number of buckets to build.
 #'
-#' @return list of lists of same structure as train.Y, but with names indicating the bucket.
+#' @return named vector
 #'
+#' @examples
+#' some.mat <- matrix(1:20, nrow = 2)
+#' colnames(some.mat) <- paste0("some.mat", 1:ncol(some.mat))
+#' n.folds <- 5
+#' mapped.buckets <- make_buckets(
+#'   train.Y = some.mat
+#'   , folds = n.folds
+#'   )
+#' table(mapped.buckets)
 make_buckets <- function(train.Y, folds) {
   bucket.indicator <- sample(
     rep(1:folds
