a3f6dad6b731751ca8a53309797ab9f3e57b5c56
Marian Schön <marian.schoen@stud.uni-regensburg.de>
2020-07-29 12:29:13 +0200

    move development into branch

--- development/from_spanglab_to_marian.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-PATH_TO="/Users/marian/Documents/"
-SOURCE="${PATH_TO}/git/DTD_spanglab"
-TARGET="${PATH_TO}/git/DTD"
-
-rsync -av ${SOURCE}/ ${TARGET}/ --exclude-from=exclude_from_rsync
